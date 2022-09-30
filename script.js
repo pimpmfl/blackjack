3bb8a9b0974d6766d45312ea05fab8d23fa35711
pimpmfl <danielpopescu934@gmail.com>
2022-09-30 15:15:30 +0200

    finished game logic

--- script.js
+++ script.js
@@ -5,7 +5,8 @@ var playerSum = 0;
 var dealerAceCount = 0;
 var playerAceCount = 0;
 var canHit = true;
-var canStay = true;   
+var canStay = true;
+var winResult = true;    
 var deck;  
 
 startGame();
@@ -18,7 +19,7 @@ function startGame() {
  cardImg.src = `./deckimg/${dealerCard.value}${dealerCard.suit}.png`
  document.getElementById("dealer-cards").append(cardImg); 
  dealerSum += getValue(dealerCard);
- 
+ dealerAceCount += checkAces(dealerCard);
  
  for(let i = 0; i < 2; i++) {
   var playerCardImg = document.createElement("img");
@@ -26,11 +27,19 @@ function startGame() {
   playerCardImg.src = `./deckimg/${playerCard.value}${playerCard.suit}.png`
   document.getElementById("player-cards").append(playerCardImg);
   playerSum += getValue(playerCard);
+  playerAceCount += checkAces(playerCard);
+  if(reduceAces(playerSum, playerAceCount)) {
+    playerSum -= 10;
+    playerAceCount -= 1; 
+  }
  }
  
  updateScore();
- if(playerSum >= 21) {
-  stay();
+ if(playerSum == 21) {
+  canHit = false; 
+  canStay = false;
+  winResult = false;  
+  document.getElementById("result").innerText = "BLACKJACK!!!";
   return; 
   } 
 
@@ -52,11 +61,15 @@ function newGame() {
   dealerSum = 0;
   playerSum = 0;
   canHit = true;
-  canStay = true;   
-  
+  canStay = true;
+  winResult = true;   
+  dealerAceCount = 0;
+  playerAceCount = 0; 
+
   document.getElementById("player-cards").innerText = "";
   document.getElementById("player-sum").innerText = ""; 
-  document.getElementById("dealer-sum").innerText = ""; 
+  document.getElementById("dealer-sum").innerText = "";
+  document.getElementById("result").innerText = ""; 
   
   updateScore(); 
   startGame();
@@ -70,12 +83,17 @@ function hit() {
   cardImg.src = `./deckimg/${playerCard.value}${playerCard.suit}.png`
   document.getElementById("player-cards").append(cardImg);
   playerSum += getValue(playerCard);
+  playerAceCount += checkAces(playerCard);
+  if(reduceAces(playerSum, playerAceCount)) {
+    playerSum -= 10;
+    playerAceCount -= 1; 
+  }
+  console.log("player score: " + playerSum);
   updateScore(); 
 
   if(playerSum >= 21) {
     stay();
   } 
-  
   
 }
 
@@ -110,10 +128,14 @@ function getValue(card) {
 
 function dealersTurn() {
   var hiddenCard = deck.pop();
-  console.log("hidden card: " + hiddenCard.value + hiddenCard.suit);
   var setHidden = document.getElementById("hidden");
   setHidden.src = `./deckimg/${hiddenCard.value}${hiddenCard.suit}.png`;
   dealerSum += getValue(hiddenCard);
+  dealerAceCount += checkAces(hiddenCard);
+  if(reduceAces(dealerSum, dealerAceCount)) {
+    dealerSum -= 10;
+    dealerAceCount -= 1; 
+  }
   updateScore();
 
   while(dealerSum < 17) {
@@ -122,8 +144,46 @@ function dealersTurn() {
     cardImg.src = `./deckimg/${dealerCard.value}${dealerCard.suit}.png`
     document.getElementById("dealer-cards").append(cardImg); 
     dealerSum += getValue(dealerCard);
+    dealerAceCount += checkAces(dealerCard);
+    if(reduceAces(dealerSum, dealerAceCount)) {
+      dealerSum -= 10;
+      dealerAceCount -= 1; 
+    }
   }
   
   updateScore();
+  var result = winner(playerSum, dealerSum)
+  document.getElementById("result").innerHTML = result;
+}
 
+function checkAces(card) {
+  if(card.value == 'A') {
+    return 1; 
+  }
+  return 0; 
+}
+
+function reduceAces(score, aceCount) {
+  return (score > 21 && aceCount > 0); 
+}
+
+function winner(playerSum, dealerSum) {
+  var result;
+  if(winResult) { 
+    if(playerSum > 21) {
+      return result = "You busted. Dealer wins.";
+    }
+    if(playerSum > dealerSum && dealerSum < 21) {
+      return result = "Your score is higher. You win.";
+    }
+    if(dealerSum > 21) {
+      return result = "Dealer busted. You win.";
+    }
+    if(dealerSum > playerSum) {
+      return result = "Dealer's score is higher. Dealer wins."
+    }
+    if(playerSum == dealerSum) {
+      return result = "Push.";
+    }
+  }
 }
